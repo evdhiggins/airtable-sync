7e61b53713cfa546cb933b68ef7c994eaee6e4a1
Evan Higgins <evdhiggins@gmail.com>
2019-01-31 21:22:30 -0600

    Fitler actions are Actions, not ActionFunctions

--- src/interfaces/ISchema.ts
+++ src/interfaces/ISchema.ts
@@ -35,7 +35,7 @@ export type Filter = {
   type: "column" | "row";
   localColumn?: string;
   match: RegExp | string | Filter.MatchFunction;
-  actions: Filter.ActionFunction[];
+  actions: Filter.Action[];
   removeFromAirtableCallback?: Filter.RemoveFromAirtableCallback;
 };
 
@@ -130,12 +130,12 @@ export const ISchemaAssertions: IAssertionTests = {
       switch (true) {
         // filter.type
         case filter.type !== "column" && filter.type !== "row":
-          return "`filter.type` must be either \"column\" or \"row\"";
+          return '`filter.type` must be either "column" or "row"';
 
         // filter.localColumn
         case filter.type === "column" &&
           (typeof filter.localColumn !== "string" || !filter.localColumn):
-          return "Filters with type \"column\" must specify a valid `localColumn`";
+          return 'Filters with type "column" must specify a valid `localColumn`';
 
         // filter.match
         case !(filter.match instanceof RegExp) &&
